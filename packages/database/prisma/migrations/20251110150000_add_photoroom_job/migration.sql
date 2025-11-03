d8fba59c779e2a1c5a9acce868b243bd357c985c
MarkTenKings <mark@tenkings.co>
2025-11-03 02:54:57 -0500

    Fix Photroom migration for production

--- packages/database/prisma/migrations/20251110150000_add_photoroom_job/migration.sql
+++ packages/database/prisma/migrations/20251110150000_add_photoroom_job/migration.sql
@@ -1,10 +1,1 @@
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_enum
-    WHERE enumlabel = 'PHOTOROOM'
-      AND enumtypid = 'ProcessingJobType'::regtype
-  ) THEN
-    ALTER TYPE "ProcessingJobType" ADD VALUE 'PHOTOROOM';
-  END IF;
-END $$;
+  ALTER TYPE "ProcessingJobType" ADD VALUE IF NOT EXISTS 'PHOTOROOM';
