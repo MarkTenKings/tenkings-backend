ca56dd8164c40490067940b8995a168a887ab3d7
MarkTenKings <mark@tenkings.co>
2025-11-21 23:54:30 -0500

    Load Stripe only on pages that require it

--- frontend/nextjs-app/pages/_app.tsx
+++ frontend/nextjs-app/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { SessionProvider } from "../hooks/useSession";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
@@ -11,23 +12,18 @@ const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 const stripePromise = publishableKey ? loadStripe(publishableKey) : null;
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const componentRequiresStripe = Boolean((Component as typeof Component & { requiresStripe?: boolean }).requiresStripe);
+  const routeRequiresStripe = router.pathname.startsWith("/packs");
+  const shouldLoadStripe = componentRequiresStripe || routeRequiresStripe;
+
   const content = (
     <SessionProvider>
       <Component {...pageProps} />
     </SessionProvider>
   );
 
-  if (!stripePromise) {
-    return (
-      <div className={`${bodyFont.variable} ${displayFont.variable} ${lightningFont.variable}`}>
-        <Head>
-          <link rel="preconnect" href="https://stream.mux.com" />
-          <link rel="dns-prefetch" href="https://stream.mux.com" />
-        </Head>
-        {content}
-      </div>
-    );
-  }
+  const wrappedContent = shouldLoadStripe && stripePromise ? <Elements stripe={stripePromise}>{content}</Elements> : content;
 
   return (
     <div className={`${bodyFont.variable} ${displayFont.variable} ${lightningFont.variable}`}>
@@ -35,7 +31,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://stream.mux.com" />
         <link rel="dns-prefetch" href="https://stream.mux.com" />
       </Head>
-      <Elements stripe={stripePromise}>{content}</Elements>
+      {wrappedContent}
     </div>
   );
 }
